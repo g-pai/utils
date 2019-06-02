a867027fd55a9ab3ff91b4025be234512299ff8d
i,prasana <prasana.iyengar@gmail.com>
2019-06-01 21:09:07 -0700

    added verbose option

--- py/factorial.py
+++ py/factorial.py
@@ -6,19 +6,17 @@ TODO(prasana): DO NOT SUBMIT without a detailed description of main.
 
 import sys # sys.argv
 import argparse ## for argument parsing
-'''
-import flags
-FLAGS = flags.FLAGS
-'''
 
 def print_str(s):
-	print (s+"\n")
+	print (str(s))
 
-def fact(n):
+def fact(n, verbose):
 	if n <= 1:
-		return(n)
+		return n
 	else:
-		return(n*fact(n-1))
+		if verbose:
+			print_str(str(n)+"*"+str(n-1))
+		return (n*fact(n-1, verbose))
 
 def reverse_file(f):
 	return
@@ -26,25 +24,19 @@ def reverse_file(f):
 def revese_str(s):
 	return v
 
-## deprecated
-def help():
-	help = sys.argv[0] 
-	help += " [--help]" 
-	help += " [--factorial=nn]"
-	print_str(help)
-
 def flagHandler():
 	parser = argparse.ArgumentParser()
 	help_str = "calculate factorial"
 	parser.add_argument("factorial", type=int, help=help_str)
+	parser.add_argument('--verbose', '-v',  action='count')
 	args = parser.parse_args()
 	return args
 
 def main(argv):
 	cli_args=flagHandler()
 	if cli_args.factorial:
-		f=fact(cli_args.factorial)
-		print_str(str(f))
+		f = fact(cli_args.factorial, cli_args.verbose)
+		print_str(f)
 	return
 
 ## main loop
