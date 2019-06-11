2e3b58c70c79c255570231cdd1a80f814eccd2ae
i,prasana <prasana.iyengar@gmail.com>
2019-06-11 13:04:05 -0700

    2nd version

--- js/hellobai.js
+++ js/hellobai.js
@@ -1,14 +1,21 @@
 var name, feeling, project;
-var test = true;
+var test;
 test = confirm("Back at it?");
 while(test){
     name = prompt("who are you?");
     if (name == "") {break;}
     feeling = prompt("Hi, " + name + ", how are you?")
-    if (feeling == "good"){ break;} 
+    if (feeling == "good"){ 
+        alert(name + " is feeling " + feeling + "; we are done here.")
+        break;
+    } 
     else{
         project = prompt(name + ", what are you working on?")
-        test = confirm("Can i help with " + project + "?");
+        projectHelpNeeded = confirm("Can i help with " + project + "?");
+        if (projectHelpNeeded){ // TODO - rest of the code
+            alert("still working on developing that capability :/!");
+        } 
+        test = confirm("Back at it?");      
     }
     
 }
